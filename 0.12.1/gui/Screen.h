47b17593a50e81fa7f33800d524462ccb7063085
아포가토맨 <colombia2@naver.com>
2015-08-17 10:46:07 +0900

    Update Screen.h
    Updated it to support 0.12

--- 0.12.1/gui/Screen.h
+++ 0.12.1/gui/Screen.h
@@ -3,91 +3,85 @@
 #include <vector>
 #include "GuiComponent.h"
 
-namespace Touch {
-	class TButton;
-}
-
-namespace Controller {
-	class StickDirection;
-}
-
 class MinecraftClient;
-class IntRectangle;
-class Button;
-class TextBox;
-class GuiElement;
+class TilePos;
+class UIRenderContext;
+class InputMode;
+class DirectionId;
 class MCOEvent;
-class MojangConnectionStatus;
+class MojangconnectionStatus;
+class Button;
+class GuiElement;
+class StickDirection;
 class Font;
 
-// Size : 212
-class Screen : public GuiComponent
-{
-public:
-	int width;								// 64
-	int height;								// 68
-	char filler1[4];						// 72
-	MinecraftClient* mc;					// 76
-	std::vector<Button*> buttonList;		// 80
-	std::vector<TextBox*> textboxList;		// 92
-	std::vector<Button*> buttonList2;		// 104
-	std::vector<GuiElement*> elementList;	// 116
-	std::vector<GuiElement*> elementList2;	// 128
-	char filler5[12];						// 140
-	Font* fontObj;							// 152
-	Button* selectedButton;					// 156
-	char filler6[28];						// 160
-	mce::MaterialPtr ui_cubemap;					// 188
-
-public:
+//Size : 212
+class Screen : public GuiComponent {
+	public:
+	char filler1[8];						//72
+	int width;								//80
+	int height;								//84
+	MinecraftClient* mc;					//88
+	std::vector<Button*> buttonList;		//92
+	char filler2[36];						//104
+	std::vector<GuiElement*> elementlist;	//140
+	char filler3[12];						//152
+	Font* fontObj;							//164
+	Button* selectedButton;					//168
+	char filler4[28];						//172
+	mce::MaterialPtr ui_cubemap;			//200
+	
+	public:
 	Screen();
 	virtual ~Screen();
+	virtual void _init(MinecraftClient&, int, int);
+	virtual void setSize(int, int);
+	virtual void onSetKeyboardHeight(int);
+	virtual void setAssociatedTilePos(TilePos const&);
+	virtual void lostFocus();
+	virtual void removed();
+	virtual void onInternetUpdate();
+	virtual void tick();
+	virtual void updateEvents();
+	virtual void render(UIRenderContext&, int, int, float);
+	virtual void handleInputModeChanged(InputMode);
+	virtual void handleButtonPress(MinecraftClient*, short);
+	virtual void handleButtonRelease(MinecraftClient*, short);
+	virtual void handlePointerLocation(short, short);
+	virtual void handlePointerPressed(bool);
+	virtual void handleDirection(DirectionId, float, float);
+	virtual bool handleBackEvent(bool);
+	virtual void handleTextChar(std::string const&);
+	virtual void setTextboxText(std::string const&);
+	virtual void handleLicenseChanged();
+	virtual bool renderGameBehind() const;
+	virtual bool closeOnPlayerHurt() const;
 	virtual void render(int, int, float);
 	virtual void init();
 	virtual void setupPositions();
-	virtual void updateEvents();
-	virtual void mouseEvent();
-	virtual void keyboardEvent();
-	virtual void keyboardTextEvent();
+	virtual void checkForPointerEvent();
 	virtual void controllerEvent();
-	virtual bool handleBackEvent(bool);
-	virtual void tick();
-	virtual void removed();
 	virtual void renderBackground(int);
 	virtual void renderDirtBackground();
 	virtual void renderMenuBackground(float);
-	virtual bool renderGameBehind();
-	virtual void hasClippingArea(IntRectangle&);
-	virtual bool isPauseScreen();
-	virtual bool isErrorScreen();
-	virtual bool isInGameScreen();
-	virtual bool closeOnPlayerHurt();
 	virtual void confirmResult(bool, int);
-	virtual void lostFocus();
 	virtual void toGUICoordinate(int&, int&);
 	virtual void feedMCOEvent(MCOEvent);
-	virtual void supppressedBySubWindow();
+	virtual bool supppressedBySubWindow();
 	virtual void onTextBoxUpdated(int);
-	virtual void onMojangConnectorStatus(MojangConnectionStatus);
-	virtual void setTextboxText(std::string const&);
-	virtual void onInternetUpdate();
+	virtual void onMojangConnectorStatus(MojangconnectionStatus);
+	virtual void handleScrollWheel(float);
+	virtual void handlePointerAction(int, int, bool);
 	virtual void updateTabButtonSelection();
 	virtual void buttonClicked(Button*);
 	virtual void guiElementClicked(GuiElement*);
-	virtual void mouseClicked(int, int, int);
-	virtual void mouseReleased(int, int, int);
-	virtual void controllerDirectionChanged(int, Controller::StickDirection);
-	virtual void controllerDirectionHeld(int, Controller::StickDirection);
-	virtual void keyPressed(int);
-	virtual void keyboardNewChar(std::string const&, bool);
-	virtual void shouldSendAllKeyStates();
-	void _init(MinecraftClient&, int, int);
-	void getCursorMoveThrottle();
-	void getPassGuiEvents();
-	void processControllerDirection(int);
-	void setPassGuiEvents(bool);
-	void setSize(int, int);
+	virtual void pointerPressed(int, int);
+	virtual void pointerReleased(int, int);
+	virtual void controllerDirectionChanged(int, StickDirection);
+	virtual void controllerDirectionHeld(int, StickDirection);
 	void tabNext();
 	void tabPrev();
-	void renderProgressBar(float) const;
+	void renderProgressBar(float);
+	void processControllerDirection(int);
+	void getCursorMoveThrottle();
 };
