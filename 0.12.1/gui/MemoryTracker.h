93fe76383ef4dac74411f1c67cfcff6849db2c42
아포가토맨 <colombia2@naver.com>
2015-08-19 09:13:18 +0900

    Create MemoryTracker.h

---
+++ 0.12.1/gui/MemoryTracker.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class MemoryTracker {
+    public:
+	//void** vtable;
+	
+    public:
+	MemoryTracker(std::string const&, MemoryTracker*);
+	virtual ~MemoryTracker();
+	virtual void getStats() const = 0;
+	void memoryStats();
+};
