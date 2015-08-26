759d39d5e382815a3869f4addf4a0f59ac2dcd62
아포가토맨 <colombia2@naver.com>
2015-08-26 16:21:58 +0900

    Create GuiElementContainer.h

---
+++ 0.12.1/gui/GuiElementContainer.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <memory>
+#include "GuiElement.h"
+
+//Size : 140
+class GuiElementContainer : public GuiElement {
+    public:
+    std::vector<GuiElement*> children;          //120
+    char filler1[8];                            //132
+    
+    public:
+    GuiElementContainer(bool, bool, int, int, int, int);
+    virtual ~GuiElementContainer();
+    virtual void tick(MinecraftClient*);
+    virtual void render(MinecraftClient*, int, int);
+    virtual void topRender(MinecraftClient*, int, int);
+    virtual void setupPositions();
+    virtual void pointerPressed(MinecraftClient*, int, int);
+    virtual void pointerReleased(MinecraftClient*, int, int);
+    virtual void focusedMouseClicked(MinecraftClient*, int, int);
+    virtual void focusedMouseReleased(MinecraftClient*, int, int);
+    virtual void handleButtonPress(MinecraftClient*, short);
+    virtual void handleTextChar(MinecraftClient*, std::string const&, bool);
+    virtual bool backPressed(MinecraftClient*, bool);
+    virtual void setKeyboardHeight(MinecraftClient*, int);
+    virtual bool suppressOtherGUI();
+    virtual void setTextboxText(std::string const&);
+    virtual bool hasChildren() const;
+    virtual void addChild(std::shared_ptr<GuiElement>);
+    virtual void removeChild(std::shared_ptr<GuiElement>);
+    virtual void clearAll();
+    std::vector<GuiElement*> getChildren();
+    std::vector<GuiElement*> getChildren() const;
+};
