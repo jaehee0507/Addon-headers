08855401cff011f938b15d23f34a78971d6b100e
affogatoman <colombia2@naver.com>
2015-09-20 11:45:57 +0900

    Fixed offset error

--- 0.12.1/entity/Abilities.h
+++ 0.12.1/entity/Abilities.h
@@ -4,12 +4,13 @@ class CompoundTag;
 
 class Abilities {
     public:
-    bool invulnerable;
-    bool flying;
-    bool mayfly;
-    bool instabuild;
-    float flyingSpeed;
-    float walkingSpeed;
+    bool invulnerable;            //0
+    bool flying;                  //1
+    bool mayfly;                  //2
+    bool instabuild;              //3
+    int filler;                   //4
+    float flyingSpeed;            //8
+    float walkingSpeed;           //12
     
     public:
     Abilities();
