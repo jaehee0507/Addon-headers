9cad1377a0acd53c6270411bf4cd84585e4d194d
affogatoman <colombia2@naver.com>
2015-09-21 22:48:10 +0900

    Fixed Some Offsets

--- 0.12.1/util/TextureData.h
+++ 0.12.1/util/TextureData.h
@@ -5,14 +5,18 @@ namespace mce {
     class Texture;
 }
 
+//Size : 88
 class TextureData {
     public:
     int width;                     //0
     int height;                    //4
     char* pixels;                  //8
+    char filler1[8];               //12
     bool idk;                      //20
+    char filler2[3];               //21
     //mce::Texture texture;        //24
-    char filler1[64];              //24
+    //std::vector<ImageData>;      //76
+    char filler3[64];              //24
     
     public:
     TextureData();
