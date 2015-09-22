aabccdbea1e373393aef52335016f068da04f832
아포가토맨 <colombia2@naver.com>
2015-09-22 20:20:52 +0900

    Create SkinInfoData.h

---
+++ 0.12.1/client/skins/SkinInfoData.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+
+class TextureData;
+
+class SkinInfoData {
+    public:
+    //void** vtable;                //0
+    bool isSlim;                    //4
+    
+    public:
+    SkinInfoData(bool);
+    virtual ~SkinInfoData();
+    virtual void updateSkin(std::string const&) = 0;
+    virtual TextureData* getTextureData();
+    virtual char* getData() = 0;
+    virtual int getDataLength() = 0;
+    virtual bool hasValidTexture() = 0;
+    virtual void validataAndResizeSkinData(std::string&);
+    void copySkinPart(std::string&, int, int, int, int, int, int, int, int);
+    bool isSlim();
+    void setForceAlpha(std::string&, int, int, int, int);
+    void setNoAlpha(std::string&, int, int, int, int);
+};
